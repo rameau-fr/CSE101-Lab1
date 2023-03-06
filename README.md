181b542038ca1f97adf6d20cd193171d046ba73f
rameau-fr <rameau.fr@gmail.com>
2023-03-06 11:06:11 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Lab 1: Arithmetic in Python
+
+Welcome to your very first lab of CSE101! Today, we will learn how to create your first Python code, to declare different types of variable and to perform basic arithmetic operations.
+
+All the details for this lab can be found in the attached ppt file!
+
+Good luck
---
+++ quad.py
@@ -0,0 +1,22 @@
+#--------------------------------------------------Task 1---------------------------------------------------
+# You're free to change these values, but some combinations won't work.
+a = 1
+b = -3
+c = 1
+
+x1 = ... # TODO: write a code to compute the first root of the quadratic equation
+x2 = ... # TODO: then do the same for the second root
+# Note: Make sure to remove the ellipsis (...) when you're writing your code
+
+print("First root:" + str(x1))
+print("Second root:" + str(x2))
+
+#--------------------------------------------------Task 2---------------------------------------------------
+# TODO: Declare two variables x and y, and initialize them to whatever positive values you like
+
+# TODO: Write a code that displays the result of division x / y
+#       The output should consist of two lines: the first line is the quotient, the second line is the remainder
+#       ex) The values of x=10 and y=3 should print out the following:
+#            3
+#            1
+# You can declare as many variables as you need.
